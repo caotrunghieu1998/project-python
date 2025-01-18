24f2f10e6666b2aa0ed30c3344c0543af691c074
lamisreal <caothanhlam29101999@gmail.com>
2025-01-18 14:59:15 +0700

    change: change file

--- client/controllers/LopController.py
+++ client/controllers/LopController.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from models.LopModel import LopModel
-from views.lopView import LopView
+from client.views.lopView2 import LopView
 
 class LopController:
     _instance = None
